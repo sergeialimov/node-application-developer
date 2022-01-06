bd59982381e3cfcf21ce0c4e13afceb648e3fa48
sergeialimov <8912652@gmail.com>
2022-01-06 17:53:46 +0500

    add modules

---
+++ 07-modules/format.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const upper = (str) => {
+  if (typeof str === 'symbol') {
+    str = str.toString();
+  }
+  str += '';
+  return str.toUpperCase();
+}
+
+module.exports = { upper };
---
+++ 07-modules/resolve-demo.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.log();
+console.group('# package resolution');
+console.log(`require('pino')`, '\t', ' =>', require.resolve('pino'));
+console.log(`require('standard')`, '\t', ' =>', require.resolve('standard'));
+console.groupEnd('');
+console.log();
+
+console.group('# directory resolution');
+console.log(`require('.')`, '\t\t', ' =>', require.resolve('.'));
+console.log(`require('../07-modules')`, ' =>', require.resolve('../07-modules'));
+console.groupEnd();
+console.log();
+
+console.group('# file resolution');
+console.log(`require('.')`, ' =>', require.resolve('./format'));
+console.log(`require('./format.js')`, ' =>', require.resolve('./format'));
+console.groupEnd();
+console.log();
+
+
+console.group('# core APIs solution');
+console.log(`require('fs')`, '\t', ' =>', require.resolve('fs'));
+console.log(`require('util')`, '\t', ' =>', require.resolve('util'));
+console.groupEnd();
+console.log();
