c870ea3fd93bb65ca7d67e0d103cf5c21c0ad634
sergeialimov <8912652@gmail.com>
2022-02-02 08:57:10 +0500

    add codify

---
+++ src/10-handling-errors/7-codify.js
@@ -0,0 +1,37 @@
+function codify (err, code) {
+  err.code = code
+  return err
+}
+
+function doTask (amount) {
+  if (typeof amount !== 'number') throw codify(
+    new TypeError('amount must be a number'),
+    'ERR_AMOUNT_MUST_BE_NUMBER'
+  )
+  if (amount <= 0) throw codify(
+    new RangeError('amount must be greater than zero'),
+    'ERR_AMOUNT_MUST_EXCEED_ZERO'
+  )
+  if (amount % 2) throw new OddError('amount')
+  return amount/2
+}
+
+try {
+  const result = doTask(4)
+  /**
+   * erroneously calling result as a function will cause the error checks
+   * to reach the final else branch in the catch block:
+   */
+  result() 
+  console.log('result', result)
+} catch (err) {
+  if (err.code === 'ERR_AMOUNT_MUST_BE_NUMBER') {
+    console.error('wrong type')
+  } else if (err.code === 'ERRO_AMOUNT_MUST_EXCEED_ZERO') {
+    console.error('out of range')
+  } else if (err.code === 'ERR_MUST_BE_EVEN') {
+    console.error('cannot be odd')
+  } else {
+    console.error('Unknown error', err)
+  }
+}
