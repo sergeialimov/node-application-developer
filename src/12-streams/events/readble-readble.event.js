22a97052f9b8ddda601cff0adb7cd380dc7c50f4
sergeialimov <8912652@gmail.com>
2022-02-10 16:45:54 +0500

    add readble event

---
+++ src/12-streams/events/readble-readble.event.js
@@ -0,0 +1,16 @@
+/**
+ * The 'readble' event is emitted when there is no data to be read from the stream or when the end of the stream has been reached. 
+ * Effectively, the 'readble' event indicates that the stream has no new information.
+ * If data is available, stream.read() will return that data
+ */
+
+
+const readble = getReadableSomehow();
+readable.on('readable', function() {
+  // There is some data to read now.
+  let data;
+  
+  while(data = this.read()) {
+    console.log(data)
+  }
+})
