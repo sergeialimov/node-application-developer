f4d34aa7bdbf0ef44e7433fbe3df4547079b9b0b
sergeialimov <8912652@gmail.com>
2022-02-12 20:41:16 +0500

    add data event

---
+++ src/12-streams/events/data.readable.event.js
@@ -0,0 +1,7 @@
+const readable = getReadableStreamSomehow();
+readable.on('data', (chunk) => {
+  console.log(`Received ${chunk.length} bytes of data.`);
+});
+readable.on('end', () => {
+  console.log('There will be no more data.');
+});
