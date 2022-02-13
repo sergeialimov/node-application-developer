4b9991602db67869f3ba8271234e8ecb15dc015d
sergeialimov <8912652@gmail.com>
2022-02-13 07:28:29 +0500

    clean up

---
+++ src/12-streams/types/Transform/Transform2.js
@@ -0,0 +1,18 @@
+/**
+ * The Transform constructor inherits from the Duplex constructor.
+ * Transform streams are duplex streams with an additional constraint applied to enforce a causal
+ * relationship between the read and write interfaces. A good example is compression:
+ */
+'use strict'
+
+const { createGzip } = require('zlib')
+
+const transform = createGzip()
+transform.on('data', (data) => {
+  console.log('got gzip data', data.toString('base64'))
+})
+
+transform.write('first')
+setTimeout(() => {
+  transform.end('second')
+}, 500)
