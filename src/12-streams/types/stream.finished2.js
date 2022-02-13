03ba253352693f6cb735f7febd22ce0aedf732f1
sergeialimov <8912652@gmail.com>
2022-02-13 09:26:53 +0500

    add finished example

---
+++ src/12-streams/types/stream.finished2.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const net = require('net')
+
+const { finished } = require('stream')
+net.createServer((socket) => {
+  const interval = setInterval(() => {
+    socket.write('beat')
+  }, 1000)
+  socket.on('data', (data) => {
+    socket.write(data.toString().toUpperCase())
+  })
+  finished(socket, (err) => {
+    if (err) {
+      console.log('there was a socket error', err)
+    }
+    clearInterval(interval)
+  })
+}).listen(3000)
