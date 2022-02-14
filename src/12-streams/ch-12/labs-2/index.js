699fb4617cf9cddb51e0e8fd84832cdc38510df6
sergeialimov <8912652@gmail.com>
2022-02-14 12:50:07 +0500

    add solution for lab-2

--- src/12-streams/ch-12/labs-2/index.js
+++ src/12-streams/ch-12/labs-2/index.js
@@ -1,6 +1,8 @@
 'use strict'
+
 const { Readable, Writable, Transform, PassThrough, pipeline } = require('stream')
 const assert = require('assert')
+
 const createWritable = () => {
   const sink = []
   const writable = new Writable({
@@ -12,13 +14,23 @@ const createWritable = () => {
   writable.sink = sink
   return writable
 }
+
 const readable = Readable.from(['a', 'b', 'c'])
 const writable = createWritable()
 
+
+const createTransformStream = () => {
+  return new Transform({
+    transform(chunk, enc, cb) {
+      const res = chunk.toString().toUpperCase()
+      cb(null, res)
+    }
+  })
+}
 // TODO: replace the pass through stream 
 // with a transform stream that uppercases
 // incoming characters
-const transform = new PassThrough()
+const transform = createTransformStream() // new PassThrough
 
 pipeline(readable, transform, writable, (err) => {
   assert.ifError(err)
