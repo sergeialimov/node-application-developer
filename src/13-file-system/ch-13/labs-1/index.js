d39e4e545d49c3a982b71053080fdd13bfec18da
sergeialimov <8912652@gmail.com>
2022-02-17 11:47:00 +0500

    add solution for lab-1

---
+++ src/13-file-system/ch-13/labs-1/index.js
@@ -0,0 +1,34 @@
+'use strict'
+const assert = require('assert')
+const { join, basename } = require('path')
+const fs = require('fs')
+const project = join(__dirname, 'project')
+try { fs.rmdirSync(project, {recursive: true}) } catch (err) {}
+const files = Array.from(Array(5), () => {
+  return join(project, Math.random().toString(36).slice(2))
+})
+files.sort()
+fs.mkdirSync(project)
+for (const f of files) fs.closeSync(fs.openSync(f, 'w'))
+
+const out = join(__dirname, 'out.txt')
+
+function exercise () {
+  // TODO read the files in the project folder
+  // and write the to the out.txt file
+  const files = fs.readdirSync('./project')
+  let content = '';
+  for (const name of files) {
+    console.log('file', name)
+    content += `${name},\n`
+  }
+
+  fs.writeFileSync(out, content.trim().slice(0, -1))
+}
+
+exercise()
+assert.deepStrictEqual(
+  fs.readFileSync(out).toString().split(',').map((s) => s.trim()),
+  files.map((f) => basename(f))
+)
+console.log('passed!')
