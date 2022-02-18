5f2ee5fafac6f2d9e771f5b2713d0f9371240329
sergeialimov <8912652@gmail.com>
2022-02-18 20:46:51 +0500

    add process.info

---
+++ src/14-process-os/process.info.js
@@ -0,0 +1,7 @@
+'use strict'
+
+console.log('current directory', process.cwd())
+console.log('dirname', __dirname)
+console.log('process platform', process.platform)
+console.log('process id', process.pid)
+console.log('process.env', process.env)
