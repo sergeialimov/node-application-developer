78a0f8d9cf328e2e31acc159412cb5bf38ce5014
sergeialimov <8912652@gmail.com>
2022-02-18 15:49:32 +0500

    add stdin stdout example

---
+++ src/14-process-os/example.js
@@ -0,0 +1,18 @@
+'use strict'
+
+console.log(process.stdin.isTTY ? 'terminal' : 'piped to')
+
+const { Transform } = require('stream')
+
+const createUppercaseStream = () => {
+  return new Transform({
+    transform(chunk, end, next) {
+      const uppercased = chunk.toString().toUpperCase()
+      next(null, uppercased)
+    }
+  })
+}
+
+const uppercase = createUppercaseStream()
+
+process.stdin.pipe(uppercase).pipe(process.stdout)
