5e51ac70bb644acd9d61aa2e12d09a2a1fafceaa
sergeialimov <8912652@gmail.com>
2022-04-11 22:01:08 +0300

    update example of multiple events adds and calls

--- src/09-events/4-multiple-once.js
+++ src/09-events/4-multiple-once.js
@@ -2,12 +2,16 @@ const EventEmitter = require('events')
 const ee = new EventEmitter()
 
 ee.on('my-ev', () => { console.log('my-ev fired') })
+ee.on('my-ev', () => { console.log('my-ev fired') })
 ee.emit('my-ev')
 ee.emit('my-ev')
 ee.emit('my-ev')
+// my-ev x6 times
 
-
+ee.once('my-e', () => { console.log('my-e')})
 ee.once('my-e', () => { console.log('my-e')})
 ee.emit('my-e')
 ee.emit('my-e')
 ee.emit('my-e')
+
+// my-e x2 times
