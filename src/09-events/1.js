6872febb9ed4b70ba78ba6208a2b29f5f9a6d586
sergeialimov <8912652@gmail.com>
2022-04-11 21:51:14 +0300

    update comments about buffers

--- src/09-events/1.js
+++ src/09-events/1.js
@@ -1,8 +1,13 @@
 const EventEmitter = require('events')
-// const { EventEmitter } = require('events') // also works
 
-// const emitter = new EventEmitter() // also works
+// In modern node the events module is the EventEmitter constructor as well:
+// const { EventEmitter } = require('events')
 
+
+// To create a new event emitter, call the constructor with new:
+// const emitter = new EventEmitter()
+
+// A more typical pattern of usage with EventEmitter, however, is to inherit from it:
 class emitter extends EventEmitter {
   constructor(opts = {}) {
     super(opts)
--- src/11-buffer/05-string-decoder.js
+++ src/11-buffer/05-string-decoder.js
@@ -2,8 +2,11 @@ const { StringDecoder } = require('string_decoder')
 
 const frag1 = Buffer.from('f09f', 'hex')
 const frag2 = Buffer.from('9180', 'hex')
+
 console.log('frag1', frag1.toString())
 console.log('frag2', frag2.toString())
+
 const decoder = new StringDecoder()
+
 console.log(decoder.write(frag1))
 console.log(decoder.write(frag2))
